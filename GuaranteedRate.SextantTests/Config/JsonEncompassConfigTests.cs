7a8b0997b917dfad01cd3e5f32a671ced5932bf2
Brian Begy <brian.begy@guaranteedrate.com>
2016-09-08 12:00:19 -0500

    formatting fixes.

--- GuaranteedRate.SextantTests/Config/JsonEncompassConfigTests.cs
+++ GuaranteedRate.SextantTests/Config/JsonEncompassConfigTests.cs
@@ -9,11 +9,14 @@ namespace GuaranteedRate.Sextant.Config.Tests
         [Test()]
         public void ForValidConfigReturnGoodValues()
         {
-            var json = "{\"foo\": \"bar\",\"boolkey\": \"false\",\"intkey\": \"4563\", \"Items\": [{\"FirstKey\": \"FirstVal\"}, {\"SecondKey\": \"SecondVal\"}] }";
+            var json =
+                "{\"foo\": \"bar\",\"boolkey\": \"false\",\"intkey\": \"4563\", \"Items\": [{\"FirstKey\": \"FirstVal\"}, {\"SecondKey\": \"SecondVal\"}] }";
             var config = new JsonEncompassConfig(json);
-            Assert.That(config.GetKeys().Count ==8, String.Format("Expected 8, got {0}",config.GetKeys().Count));
-            Assert.That(config.GetValue("foo").Equals("bar"), String.Format("Expected 3, got {0}", config.GetValue("foo")));
-            Assert.That(config.GetValue("boolkey",true).Equals(false),String.Format("Expected 3, got {0}", config.GetValue("boolkey", true)));
+            Assert.That(config.GetKeys().Count == 8, String.Format("Expected 8, got {0}", config.GetKeys().Count));
+            Assert.That(config.GetValue("foo").Equals("bar"),
+                String.Format("Expected 3, got {0}", config.GetValue("foo")));
+            Assert.That(config.GetValue("boolkey", true).Equals(false),
+                String.Format("Expected 3, got {0}", config.GetValue("boolkey", true)));
             Assert.That(config.GetValue("Items.[0].FirstKey").Equals("FirstVal"));
         }
     }
