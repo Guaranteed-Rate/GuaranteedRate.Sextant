fc5459228ffc3833e9013e2618bc521f30cb88ce
Brian Begy <brian.begy@guaranteedrate.com>
2017-01-20 08:34:23 -0600

    expose loan guid search functionality.

--- GuaranteedRate.Sextant/EncompassUtils/SessionUtils.cs
+++ GuaranteedRate.Sextant/EncompassUtils/SessionUtils.cs
@@ -28,12 +28,18 @@ namespace GuaranteedRate.Sextant.EncompassUtils
             return session.Loans.Open(guid);
         }
 
-        public static Loan OpenLoanFromLoanNumber(Session session, string loan_number)
+        /// <summary>
+        /// returns the guid for a given loan_number, using Encompass's search function
+        /// </summary>
+        /// <param name="session">an active Encompass session</param>
+        /// <param name="loan_number">loan number of the loan</param>
+        /// <returns>the guid (as a string) or String.Empty if not found.</returns>
+        public static string GetGuidForLoanNumber(Session session, string loan_number)
         {
             StringFieldCriterion sfc = new StringFieldCriterion("Loan.LoanNumber", loan_number,
-                StringFieldMatchType.Exact, true);
+            StringFieldMatchType.Exact, true);
             LoanIdentityList loanList = session.Loans.Query(sfc);
-            string guid = null;
+            string guid = String.Empty;
 
             //Loan.LoanNumber SHOULD be unique, but there is a timing bug
             //If multiple loans found, retun the last one
@@ -41,6 +47,14 @@ namespace GuaranteedRate.Sextant.EncompassUtils
             {
                 guid = id.Guid;
             }
+
+            return guid;
+
+        }
+
+        public static Loan OpenLoanFromLoanNumber(Session session, string loan_number)
+        {
+            var guid = GetGuidForLoanNumber(session, loan_number);
             if (!String.IsNullOrWhiteSpace(guid))
             {
                 return session.Loans.Open(guid);
