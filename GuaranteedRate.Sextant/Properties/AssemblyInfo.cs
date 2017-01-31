4d5439ff428e3e15319b300269cc977d95c4e0e4
Brian Begy <brian.begy@guaranteedrate.com>
2017-01-31 13:58:12 -0600

    fix version

--- GuaranteedRate.Sextant/Properties/AssemblyInfo.cs
+++ GuaranteedRate.Sextant/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("17.1.1.0")]
-[assembly: AssemblyFileVersion("17.1.1.0")]
+[assembly: AssemblyVersion("17.1.0.0")]
+[assembly: AssemblyFileVersion("17.1.0.0")]
