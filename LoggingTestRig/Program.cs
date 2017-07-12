03c2fefaad92eca6a4b7f13ba73be180ef7f3369
Jon Gear <askjon@jongear.io>
2017-07-12 14:24:52 -0600

    cleaning

--- LoggingTestRig/Program.cs
+++ LoggingTestRig/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GuaranteedRate.Sextant;
 using GuaranteedRate.Sextant.Config;
 using GuaranteedRate.Sextant.Logging;
 using GuaranteedRate.Sextant.Logging.Elasticsearch;
