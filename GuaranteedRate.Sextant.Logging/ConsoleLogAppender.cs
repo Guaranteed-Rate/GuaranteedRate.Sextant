4fe41e309dcf1a37c0e5b05c2f6a8d89c6614bae
Jon Gear <askjon@jongear.io>
2017-07-13 13:16:13 -0600

    cleaning

--- GuaranteedRate.Sextant.Logging/ConsoleLogAppender.cs
+++ GuaranteedRate.Sextant.Logging/ConsoleLogAppender.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GuaranteedRate.Sextant.Config;
 
 namespace GuaranteedRate.Sextant.Logging
@@ -10,45 +8,45 @@ namespace GuaranteedRate.Sextant.Logging
     /// <summary>
     /// trivial console appender for logs
     /// </summary>
-    public class ConsoleLogAppender:ILogAppender
+    public class ConsoleLogAppender : ILogAppender
     {
-        public void Setup(IEncompassConfig config)
-        {
-            this.DebugEnabled = config.GetValue("ConsoleLogAppender.Debug.Enabled", false);
-            this.InfoEnabled = config.GetValue("ConsoleLogAppender.Info.Enabled", false);
-            this.WarnEnabled = config.GetValue("ConsoleLogAppender.Warn.Enabled", false);
-            this.ErrorEnabled = config.GetValue("ConsoleLogAppender.Error.Enabled", true);
-            this.FatalEnabled = config.GetValue("ConsoleLogAppender.Fatal.Enabled", true);
-        }
-
-        public void Log(IDictionary<string, string> fields)
-        {
-            if (String.Equals(fields["level"], "debug", StringComparison.CurrentCultureIgnoreCase) && this.DebugEnabled)
-            {
-                Console.WriteLine(fields.Values.Aggregate((a,b)=> $"{a}, {b}"));
-            }
-            if (String.Equals(fields["level"], "info", StringComparison.CurrentCultureIgnoreCase) && this.InfoEnabled)
-            {
-                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
-            }
-            if (String.Equals(fields["level"], "warn", StringComparison.CurrentCultureIgnoreCase) && this.WarnEnabled)
-            {
-                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
-            }
-            if (String.Equals(fields["level"], "error", StringComparison.CurrentCultureIgnoreCase) && this.ErrorEnabled)
-            {
-                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
-            }
-            if (String.Equals(fields["level"], "fatal", StringComparison.CurrentCultureIgnoreCase) && this.FatalEnabled)
-            {
-                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
-            }
-        }
-
         public bool DebugEnabled { get; private set; }
         public bool InfoEnabled { get; private set; }
         public bool WarnEnabled { get; private set; }
         public bool ErrorEnabled { get; private set; }
         public bool FatalEnabled { get; private set; }
+
+        public void Setup(IEncompassConfig config)
+        {
+            DebugEnabled = config.GetValue("ConsoleLogAppender.Debug.Enabled", false);
+            InfoEnabled = config.GetValue("ConsoleLogAppender.Info.Enabled", false);
+            WarnEnabled = config.GetValue("ConsoleLogAppender.Warn.Enabled", false);
+            ErrorEnabled = config.GetValue("ConsoleLogAppender.Error.Enabled", true);
+            FatalEnabled = config.GetValue("ConsoleLogAppender.Fatal.Enabled", true);
+        }
+
+        public void Log(IDictionary<string, string> fields)
+        {
+            if (string.Equals(fields["level"], "debug", StringComparison.CurrentCultureIgnoreCase) && DebugEnabled)
+            {
+                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
+            }
+            if (string.Equals(fields["level"], "info", StringComparison.CurrentCultureIgnoreCase) && InfoEnabled)
+            {
+                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
+            }
+            if (string.Equals(fields["level"], "warn", StringComparison.CurrentCultureIgnoreCase) && WarnEnabled)
+            {
+                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
+            }
+            if (string.Equals(fields["level"], "error", StringComparison.CurrentCultureIgnoreCase) && ErrorEnabled)
+            {
+                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
+            }
+            if (string.Equals(fields["level"], "fatal", StringComparison.CurrentCultureIgnoreCase) && FatalEnabled)
+            {
+                Console.WriteLine(fields.Values.Aggregate((a, b) => $"{a}, {b}"));
+            }
+        }
     }
 }
