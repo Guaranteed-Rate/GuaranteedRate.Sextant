6cbff8d60fae3b48933348fa36dd92d005dac947
Jon Gear <askjon@jongear.io>
2017-07-13 13:09:18 -0600

    integration tests

---
+++ GuaranteedRate.Sextant.Logging.Elasticsearch.Integration.Tests/ElasticsearchLogAppenderIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using GuaranteedRate.Sextant.Integration.Core;
+using NUnit.Framework;
+
+namespace GuaranteedRate.Sextant.Logging.Elasticsearch.Integration.Tests
+{
+    [TestFixture]
+    public class ElasticsearchLogAppenderIntegrationTests
+    {
+        public ElasticsearchLogAppender _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new ElasticsearchLogAppender(new IntegrationEncompassConfig());
+        }
+
+        [Test]
+        public void WhenLog_ThenSuccess()
+        {
+            var fields = new Dictionary<string, string>
+            {
+                {"Application", "Encompass"},
+                {"Company", "Guaranteed Rate"}
+
+            };
+            _sut.Log(fields);
+        }
+    }
+}
---
+++ GuaranteedRate.Sextant.Logging.Loggly.Integration.Tests/LogglyLogAppenderIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using GuaranteedRate.Sextant.Integration.Core;
+
+namespace GuaranteedRate.Sextant.Logging.Loggly.Integration.Tests
+{
+    [TestFixture]
+    public class LogglyLogAppenderIntegrationTests
+    {
+        public LogglyLogAppender _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new LogglyLogAppender(new IntegrationEncompassConfig());    
+        }
+
+        [Test]
+        public void WhenLog_ThenSuccess()
+        {
+            var fields = new Dictionary<string, string>
+            {
+                {"Application", "Encompass"},
+                {"Company", "Guaranteed Rate"}
+
+            };
+            _sut.Log(fields);
+        }
+    }
+}
