5f3623815e92eeab93193dd10c7ae18acaa1b52f
Brian Begy <brian.begy@guaranteedrate.com>
2017-10-03 10:00:34 -0500

    remove casing gotchas

--- GuaranteedRate.Sextant.Integration.Tests/Logging/Elasticsearch/ElasticsearchLogAppenderIntegrationTests.cs
+++ GuaranteedRate.Sextant.Integration.Tests/Logging/Elasticsearch/ElasticsearchLogAppenderIntegrationTests.cs
@@ -16,6 +16,7 @@ namespace GuaranteedRate.Sextant.Integration.Tests.Logging.Elasticsearch
                 var fields = new Dictionary<string, string>
                 {
                     {"Application", "SextantLogger"},
+                    {"Message", "The original team behind the game Portal called the game Narbuncular Drop because the word Narbuncular is unique.  It would make a great test logging message too." },
                     {"Company", "Guaranteed Rate"},
                     {"loggerName", "Guaranteed Rate Encompass Logger"}
                 };
