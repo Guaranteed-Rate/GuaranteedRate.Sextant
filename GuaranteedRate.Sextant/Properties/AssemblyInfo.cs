2b2a352e1afb9129deaaf8218b8d6ec9adee7b4f
Brian Begy <brian.begy@guaranteedrate.com>
2018-02-20 12:53:15 -0600

    CL

--- GuaranteedRate.Sextant/Properties/AssemblyInfo.cs
+++ GuaranteedRate.Sextant/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("17.4.1.6")]
-[assembly: AssemblyFileVersion("17.4.1.6")]
+[assembly: AssemblyVersion("17.4.1.7")]
+[assembly: AssemblyFileVersion("17.4.1.7")]
