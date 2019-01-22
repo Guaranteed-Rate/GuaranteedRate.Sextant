ce522e9ef9c87dca9c0bc30f0ca9210b6c044831
Kris Nelson <krisn@guaranteedrate.com>
2019-01-22 15:53:43 -0600

    Integration tests and updated LoggingTestRig.

---
+++ GuaranteedRate.Sextant.Integration.Tests/Logging/SextantLoggerIntegrationTests.cs
@@ -0,0 +1,65 @@
+﻿using GuaranteedRate.Sextant.Config;
+using GuaranteedRate.Sextant.Integration.Core;
+using GuaranteedRate.Sextant.Logging;
+using NUnit.Framework;
+
+namespace GuaranteedRate.Sextant.Integration.Tests.Logging
+{
+    [TestFixture]
+    [Explicit]
+    class SextantLoggerIntegrationTests
+    {
+        private IEncompassConfig _encompassConfig;
+        private string _loggerName = "SextantLoggerIntegrationTests";
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _encompassConfig = new IntegrationEncompassConfig();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            Logger.Shutdown(30, true);
+            //Serilog.Log.CloseAndFlush();
+        }
+
+        [Test]
+        public void Constructor_with_config_only_returns_logger()
+        {
+            // act
+            var actual = new SextantLogger(_encompassConfig, _loggerName);
+
+            // assert
+            Assert.NotNull(actual);
+        }
+
+        [Test]
+        public void Warning_writes_to_loggly()
+        {
+            // arrange
+            var logger = new SextantLogger(_encompassConfig, _loggerName);
+
+            // act
+            logger.Warning("Test warning from integration test");
+
+            // assert
+            Assert.Pass("Check loggly for results");
+        }
+
+        [Test]
+        public void Info_writes_to_loggly()
+        {
+            // arrange
+            var logger = new SextantLogger(_encompassConfig, _loggerName);
+
+            // act
+            logger.Info("Test info from integration test");
+
+            // assert
+            Assert.Pass("Check loggly for results");
+        }
+
+    }
+}
