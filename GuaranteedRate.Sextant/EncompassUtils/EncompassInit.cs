97b4e1d613e28d97ad9d786fb1c0b9fc8b866226
Tyler Brinkley <tbrinkley@guaranteedrate.com>
2020-01-13 11:48:00 -0600

    Removed dependency on EncompassSDK.Complete NuGet package as well as other unnecessary packages.

--- GuaranteedRate.Sextant/EncompassUtils/EncompassInit.cs
+++ GuaranteedRate.Sextant/EncompassUtils/EncompassInit.cs
@@ -1,4 +1,7 @@
-﻿namespace GuaranteedRate.Sextant.EncompassUtils
+﻿using System;
+using System.Linq;
+
+namespace GuaranteedRate.Sextant.EncompassUtils
 {
     public class EncompassInit
     {
@@ -7,7 +10,16 @@
         /// </summary>
         public static void InitEncompass()
         {
-              new EllieMae.Encompass.Runtime.RuntimeServices().Initialize();
+            // Called through reflection so as not to require a dll reference to the EllieMae.Encompass.Runtime dll
+            // which should only be required for standalone applications, not libraries or plugins and forms code.
+            var assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == "EllieMae.Encompass.Runtime");
+            if (assembly == null)
+            {
+                throw new InvalidOperationException("Could not find assembly EllieMae.Encompass.Runtime in the current app domain");
+            }
+            var type = assembly.GetType("EllieMae.Encompass.Runtime.RuntimeServices");
+            var runtimeServices = Activator.CreateInstance(type);
+            type.GetMethod("Initialize").Invoke(runtimeServices, null);
         }
 
         /// <summary>
