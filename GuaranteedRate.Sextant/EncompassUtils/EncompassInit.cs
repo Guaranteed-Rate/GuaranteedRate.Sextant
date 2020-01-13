6f982dd02dea34df71db3432495b22952a0353ec
Tyler Brinkley <tbrinkley@guaranteedrate.com>
2020-01-13 11:36:05 -0600

    Revert "Reassessed the dependencies of Sextant. Now relies on the Encompass.Objects.SDK NuGet package instead of the EncompassSDK.Complete NuGet package."
    This reverts commit 478c1784b467a5d8fa0d4c87c0f4f1784214b621.

--- GuaranteedRate.Sextant/EncompassUtils/EncompassInit.cs
+++ GuaranteedRate.Sextant/EncompassUtils/EncompassInit.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-
-namespace GuaranteedRate.Sextant.EncompassUtils
+﻿namespace GuaranteedRate.Sextant.EncompassUtils
 {
     public class EncompassInit
     {
@@ -10,16 +7,7 @@ namespace GuaranteedRate.Sextant.EncompassUtils
         /// </summary>
         public static void InitEncompass()
         {
-            // Called through reflection so as not to require a dll reference to the EllieMae.Encompass.Runtime dll
-            // which should only be required for standalone applications, not libraries or plugins and forms code.
-            var assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == "EllieMae.Encompass.Runtime");
-            if (assembly == null)
-            {
-                throw new InvalidOperationException("Could not find assembly EllieMae.Encompass.Runtime in the current app domain");
-            }
-            var type = assembly.GetType("EllieMae.Encompass.Runtime.RuntimeServices");
-            var runtimeServices = Activator.CreateInstance(type);
-            type.GetMethod("Initialize").Invoke(runtimeServices, null);
+              new EllieMae.Encompass.Runtime.RuntimeServices().Initialize();
         }
 
         /// <summary>
